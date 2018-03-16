e1e2a4598f9d00b7c04df81c1d39bd8628af5238
Bjølseth, Are <are.bjolseth@nav.no>
2018-03-16 10:56:34 +0100

    Moved content from Bitbucket

--- scripts/mock_api_server.js
+++ scripts/mock_api_server.js
@@ -1,13 +1,63 @@
 /* eslint-disable */
 const express = require('express');
-const _ = require('underscore');
-const app = express();
-const bodyParser = require('body-parser');
-
 const fs = require('fs');
-//const path = require('path');
 const os = require('os');
+const URL = require('url');
+const bodyParser = require('body-parser');
 const moment = require('moment');
+const _ = require('underscore');
+const readableRandom = require('readable-random');
+
+const MOCK_DATA_DIR = './scripts/mock_data';
+const timestamp = moment();
+const errorMessage = (status, error, message, path) => ({
+  timestamp,
+  status,
+  error,
+  message,
+  path,
+});
+
+const badRequest400 = (path) => {
+  return errorMessage(400,
+    'Bad Request',
+    'The server cannot or will not process the request due to an apparent client error (e.g., malformed request syntax, size too large, invalid request message framing, or deceptive request routing).',
+    path);
+};
+const unauthorizedRequest401 = (path) => {
+  return errorMessage(401, 'Unauthorized',
+    'Similar to 403 Forbidden, but specifically for use when authentication is required and has failed or has not yet been provided.',
+    path);
+};
+const forbiddenRequest403 = (path) => {
+  return errorMessage(403, 'Unauthorized',
+    'The request was valid, but the server is refusing action. The user might not have the necessary permissions for a resource, or may need an account of some sort.',
+    path);
+};
+const notFound404 = (path) => {
+  return errorMessage(404, 'Not Found',
+    'The requested resource could not be found but may be available in the future. Subsequent requests by the client are permissible.',
+    path);
+};
+const serverError500 = (path) => {
+  return errorMessage(500, 'Internal Server Error',
+    'A generic error message, given when an unexpected condition was encountered and no more specific message is suitable.',
+    path);
+};
+const ERR = {
+  badRequest400,
+  unauthorizedRequest401,
+  forbiddenRequest403,
+  notFound404,
+  serverError500,
+};
+
+function happyStatus(statusarray) {
+  const HAPPY = process.env.HAPPY || false;
+  return (HAPPY) ? 200 : _.sample(statusarray);
+}
+
+const app = express();
 
 const allowCrossDomain = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -18,31 +68,171 @@ const allowCrossDomain = function (req, res, next) {
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-//app.use(bodyParser.text());
 app.use(bodyParser.raw());
 
 const port = process.env.PORT || 3002;
 const router = express.Router();
 
-router.post('/opprettsak/:fnr', function(req, res) {
-  console.log('opprettsak POST');
-  console.log('content-type', req.get('content-type'));
-  console.log('opprettsak',req.body);
-  const fnr = req.params.fnr.toString();
-  const nysak = {
-    fnr: fnr,
-    saksnummer: _.random(100,9999).toString(),
-    kjoenn: 'K',
-    sammensattNavn: 'UNNI FOS KVALUY',
-    registrertDato: moment(),
-    status: 'Ikke påbegynnt'
-  };
-  res.json(nysak);
+
+router.get('/oppgaver/hentoppgave/:oppgaveID', (req, res) => {
+  try {
+    const oppgaveID = req.params.oppgaveID;
+    const mockfile = `${MOCK_DATA_DIR}/oppgaver/oppgave-id-${oppgaveID}.json`;
+    if (fs.existsSync(mockfile)) {
+      const oppgave = JSON.parse(fs.readFileSync(mockfile, "utf8"));
+      return res.json(oppgave);
+    }
+    else {
+      console.log("File Not found: "+ mockfile);
+      const melding = ERR.notFound404(req.url);
+      return res.status(404).send(melding);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
+const lesOppgaveObjekt = () => {
+  const mockfile = `${MOCK_DATA_DIR}/oppgaver/oppgaveliste.json`;
+  const oppgaveobjekt = JSON.parse(fs.readFileSync(mockfile, "utf8"));
+  return oppgaveobjekt;
+};
+
+
+const minesaker = (oppgaveliste) => {
+  return oppgaveliste.map(oppgave => {
+    const mock = _.sample([{
+      sammensattNavn: 'LILLA HEST',
+      saksnummer: 3
+    }, {
+      sammensattNavn: 'GLITRENDE HATT',
+      saksnummer: 4
+    }]);
+    const { status, aktivTil, oppgaveId } = oppgave;
+    const { sammensattNavn, saksnummer } = mock;
+
+    return {
+      oppgaveId,
+      oppgavetype: _.sample(['behandling','journalforing']),
+      grunnlagstype: {
+        kode: 'MEDEOS',
+        term: 'Medlem, EØS-avtalen',
+      },
+      sammensattNavn,
+      saksnummer,
+      dokumentID: null,
+      status: {
+        kode: status.kode,
+        term: 'Oversett kode til display tekst',
+      },
+      aktivTil,
+      soknadsperiode: {
+        fom: '2016-01-01',
+        tom: '2020-01-01',
+      },
+    }
+  });
+};
+const finnoppgaveliste = (req, res) => {
+  try {
+    const oppgaveobjekt = lesOppgaveObjekt();
+    return res.json(oppgaveobjekt);
+  }
+  catch (err) {
+    console.log(err);
+    return res.status(500).send(err);
+  }
+};
+router.get('/oppgaver/finnoppgaveliste', finnoppgaveliste);
+
+router.get('/oppgaver', (req, res) => {
+  try {
+    const oppgaveobjekt = lesOppgaveObjekt();
+    const { oppgaveListe } = oppgaveobjekt;
+
+    //const fnummere = oppgaveListe.reduce((acc, oppgave) => [...acc, oppgave.gjelder.brukerId], []);
+    //const fnummere = oppgaveListe.reduce((acc, oppgave) => acc.includes(oppgave.gjelder.brukerId) ? acc : [...acc, oppgave.gjelder.brukerId], []);
+    //console.log(fnummere);
+
+    return res.json(oppgaveListe);
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 });
 
+// Kodeverk: https://kodeverkviewer.adeo.no/kodeverk/xml/index.html
+// fagomrade: https://kodeverkviewer.adeo.no/kodeverk/xml/fagomrade.xml
+//
+router.get('/oppgaver/plukkoppgave/:fagomrade?/:underkategori?/:oppgavetype?', (req, res) => {
+  const { fagomrade='F', underkategori='U', oppgavetype='T' } = req.params;
+  try {
+    const oppgaveobjekt = lesOppgaveObjekt();
+    const { oppgaveListe } = oppgaveobjekt;
+    const plukkliste = oppgaveListe.slice(-oppgaveListe.length, -oppgaveListe.length/2);
+    const mineoppgaver = minesaker(plukkliste) ;
+    let oppgave = _.sample(mineoppgaver);
+    oppgave.status.term = 'plukkliste';
 
+    const mockfile = `${MOCK_DATA_DIR}/oppgaver/plukkoppgave-${oppgave.oppgaveId}.json`;
+    fs.writeFileSync(mockfile, JSON.stringify(oppgave, null, 2));
+    return res.json(oppgave);
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
+const lesPlukkOppgaver = () => {
+  const mockOppgaverDir = `${MOCK_DATA_DIR}/oppgaver`;
+  let plukkOppgaver = [];
+  fs.readdirSync(mockOppgaverDir).forEach(file => {
+    if (file.startsWith('plukkoppgave')) {
+      const plukkoppgave = JSON.parse(fs.readFileSync(`${mockOppgaverDir}/${file}`, 'UTF-8'));
+      plukkOppgaver.push(plukkoppgave);
+    }
+  });
+  return plukkOppgaver;
+};
+const hentMineOppgaver = (req, res) => {
+  try {
+    const plukkoppgaver = lesPlukkOppgaver();
+    const oppgaveobjekt = lesOppgaveObjekt();
+    const { oppgaveListe } = oppgaveobjekt;
+    const firstHalf = oppgaveListe.slice(0, 4);
+    const mineoppgaver = minesaker(firstHalf);
+    return res.json([...plukkoppgaver, ...mineoppgaver]);
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+};
+router.get('/oppgaver/hentSaksoversikt', hentMineOppgaver);
+router.get('/oppgaver/hentMineOppgaver', hentMineOppgaver);
 
-
+  /**
+ * OPPRETTSAK
+ */
+router.post('/opprettsak/:fnr', function(req, res) {
+  try {
+    const fnr = req.params.fnr.toString();
+    const nysak = {
+      fnr,
+      saksnummer: _.random(100,9999).toString(),
+      kjoenn: 'K',
+      sammensattNavn: 'GRØNN ELEFANT',
+      registrertDato: moment(),
+      status: 'Ikke påbegynnt'
+    };
+    res.json(nysak);
+  }
+  catch (err) {
+    console.log(err);
+    return res.status(500).send(err);
+  }
+});
 
 /**
  * FAGSAKER
@@ -58,9 +248,16 @@ router.post('/opprettsak/:fnr', function(req, res) {
 function fagsaker_snr(req, res) {
     try {
         const snr = req.params.snr && req.params.snr.toString() || '';
-        const filnavn = "./scripts/mock_data/fagsaker/snr-"+snr+".json";
-        const fagsaker = JSON.parse(fs.readFileSync(filnavn, "utf8"));
-        return res.json(fagsaker);
+        const mockfile = `${MOCK_DATA_DIR}/fagsaker/snr-${snr}.json`;
+        if (fs.existsSync(mockfile)) {
+          const fagsaker = JSON.parse(fs.readFileSync(mockfile, "utf8"));
+          return res.json(fagsaker);
+        }
+        else {
+          console.error("File not found:"+ mockfile);
+          const melding = ERR.notFound404(req.url);
+          return res.status(404).send(melding);
+        }
     }
     catch (err) {
         console.error(err);
@@ -70,26 +267,105 @@ function fagsaker_snr(req, res) {
 
 router.get('/fagsaker/:snr', fagsaker_snr);
 
+function opprettNyFagsak(req, res) {
+  try {
+
+    const fnr = req.params.fnr && req.params.fnr.toString() || '';
+    const mockFagsakerDir = `${MOCK_DATA_DIR}/fagsaker`;
+
+    fs.readdirSync(mockFagsakerDir).forEach(file => {
+      const fagsak = JSON.parse(fs.readFileSync(`${mockFagsakerDir}/${file}`, 'UTF-8'));
+      if (fagsak.behandlinger[0].saksopplysninger.person.fnr === fnr) {
+        return res.json(fagsak);
+      }
+    });
+    /*
+    if (!funnet) {
+      console.error(`Ingen fagsak funnet for fnr=${fnr}`);
+      const error404Message = errorMessage(404,'Not Found', req.url);
+      return res.status(404).send(JSON.stringify(error404Message));
+    }
+    */
+    const fornavn = readableRandom.getString(5).toUpperCase();
+    const etternavn = readableRandom.getString(8).toUpperCase();
+    const mockfagsak = {
+      saksnummer: _.random(5,20).toString(),
+      fnr,
+      sammensattNavn: `${fornavn} ${etternavn}`,
+      type: 'A1',
+      status: 'OPPRETTET',
+      registrertDato: timestamp,
+      kjoenn: _.sample(['M','K']),
+    };
+    const mockfile = `${MOCK_DATA_DIR}/sok/fagsaker/fnr-${fnr}.json`;
+    mockfagsaker = [mockfagsak];
+    fs.writeFileSync(mockfile, JSON.stringify(mockfagsaker, null, 2));
+    return res.status(201).send(mockfagsak);
+  }
+  catch (err) {
+    console.error(err);
+    return res.status(500).send(err);
+  }
+}
+router.get('/fagsaker/ny/:fnr', opprettNyFagsak);
+
 function sokFagsaker_fnr(req, res) {
     try {
-        const fnr = req.query.fnr.toString();
-        const filnavn = "./scripts/mock_data/sok/fagsaker/fnr-"+fnr+".json";
-        const nyesaker = JSON.parse(fs.readFileSync(filnavn, "utf8"));
+      const fnr = req.query.fnr.toString();
+      const mockfile = `${MOCK_DATA_DIR}/sok/fagsaker/fnr-${fnr}.json`;
+      if (fs.existsSync(mockfile)) {
+        const nyesaker = JSON.parse(fs.readFileSync(mockfile, "utf8"));
 
         if (nyesaker && nyesaker.length) {
-            return res.json(nyesaker);
+          return res.json(nyesaker);
         }
-        else {
-            return res.status(404).send("Not found");
+      }
+      else {
+        const url = URL.parse(req.url);
+        // Søk på fagsaker med fnr kan gi tomt svar, eller gi unauthorized
+        const status = happyStatus([200, 200, 403, 500]);
+        switch (status) {
+          case 200: {
+            const tom_soknad = [];
+            return res.json(tom_soknad);
+          }
+          case 403: {
+            const melding = ERR.forbiddenRequest403(url.pathname);
+            return res.status(status).send(melding);
+          }
+          case 500: {
+            const melding = ERR.serverError500(url.pathname);
+            return res.status(status).send(melding);
+          }
         }
+
+      }
     } catch (err) {
+        console.log(err);
         res.status(500).send(err);
-        console.log(err)
     }
 }
 router.get('/sok/fagsaker', sokFagsaker_fnr);
-
-
+router.get('/sok/fagsaker/liste', (req, res) => {
+  let fagsakListe = [];
+  try {
+    const mockSokFagsakerDir = `${MOCK_DATA_DIR}/sok/fagsaker/`;
+    fs.readdirSync(mockSokFagsakerDir).forEach(file => {
+      const fagsaker = JSON.parse(fs.readFileSync(`${mockSokFagsakerDir}/${file}`, 'UTF-8'));
+      fagsaker.every((fagsak) => {
+        fagsakListe.push(fagsak);
+      })
+    });
+    fagsakListe = _.uniq(fagsakListe.sort((a, b) => {
+      return a.saksnummer - b.saksnummer;
+    }), true);
+    return res.json(fagsakListe);
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
 
 /**
  * SØKNAD
@@ -103,9 +379,15 @@ router.get('/sok/fagsaker', sokFagsaker_fnr);
 function getSoknad(req, res) {
   try {
     const behandlingID = req.params.behandlingID;
-    const filnavn = "./scripts/mock_data/soknader/soknad-bid-"+behandlingID+".json";
-    const soknad = JSON.parse(fs.readFileSync(filnavn, "utf8"));
-    return res.json(soknad);
+    const mockfileSoknad = `${MOCK_DATA_DIR}/soknader/soknad-bid-${behandlingID}.json`;
+    if (fs.existsSync(mockfileSoknad)) {
+      const soknad = JSON.parse(fs.readFileSync(mockfileSoknad, "utf8"));
+      return res.json(soknad);
+    }
+    else {
+      console.error('Not Found', mockfileSoknad);
+      return res.status(404).send(ERR.notFound404( req.url));
+    }
   }
   catch (err) {
     console.error(err);
@@ -115,12 +397,32 @@ function getSoknad(req, res) {
 router.get('/soknader/:behandlingID', getSoknad);
 
 function postSoknad(req, res) {
-    const behandlingID = req.params.behandlingID;
-    var body = req.body;
-    var jsonBody = isJSON(body) ? JSON.parse(body) : body;
-    jsonBody.behandlingID = behandlingID;
+  const behandlingID = req.params.behandlingID;
+  const body = req.body;
+  let jsonBody = isJSON(body) ? JSON.parse(body) : body;
 
-    return res.json(jsonBody);
+  const mockfileSoknad = `${MOCK_DATA_DIR}/soknader/soknad-bid-${behandlingID}.json`;
+
+  try {
+    if (fs.existsSync(mockfileSoknad)) {
+      const soknad = JSON.parse(fs.readFileSync(mockfileSoknad, "utf8"));
+      return res.json(soknad);
+    }
+    else {
+      const { soknadDokument } = jsonBody;
+      // Triks for å sikre at behandlingsID kommmer som forste key og ikke sist
+      const soknad = {
+        behandlingID,
+        soknadDokument,
+      };
+      fs.writeFileSync(mockfileSoknad, JSON.stringify(soknad, null, 2));
+      return res.json(soknad);
+    }
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
 }
 router.post('/soknader/:behandlingID', postSoknad);
 
@@ -139,10 +441,14 @@ router.post('/soknader/:behandlingID', postSoknad);
 function getFaktaavklaring(req, res) {
     try {
         const behandlingID = req.params.behandlingID;
-        var mockfile = "./scripts/mock_data/faktaavklaring/faktaavklaring-bid-"+behandlingID+".json";
-        console.log('mockfile', mockfile);
-        const faktaavklaring = JSON.parse(fs.readFileSync(mockfile, "utf8"));
-        return res.json(faktaavklaring);
+        const mockfile = `${MOCK_DATA_DIR}/faktaavklaring/faktaavklaring-bid-${behandlingID}.json`;
+        if (fs.existsSync(mockfile)) {
+          const faktaavklaring = JSON.parse(fs.readFileSync(mockfile, "utf8"));
+          return res.json(faktaavklaring);
+        }
+        else {
+          return res.status(404).send(ERR.notFound404(req.url));
+        }
     }
     catch (err) {
         console.error(err);
@@ -153,11 +459,14 @@ router.get('/faktaavklaring/:behandlingID', getFaktaavklaring);
 
 router.post('/faktaavklaring/:behandlingID', function (req, res) {
     const behandlingID = req.params.behandlingID;
-    var body = req.body;
-    var jsonBody = isJSON(body) ? JSON.parse(body) : body;
-    jsonBody.behandlingID = behandlingID;
+    const body = req.body;
+    const faktaavklaring = isJSON(body) ? JSON.parse(body) : body;
+    const faktaavklaringen = {
+      behandlingID,
+      faktaavklaring: { ...faktaavklaring }
+    };
 
-    return res.json(jsonBody);
+    return res.json(faktaavklaringen);
 });
 
 
@@ -172,12 +481,20 @@ router.post('/faktaavklaring/:behandlingID', function (req, res) {
  *
  */
 function getVurdering(req, res) {
-
     try {
-        const behandlingID = req.params.behandlingID;
-        var mockfile = "./scripts/mock_data/vurdering/vurdering-bid-"+behandlingID+".json";
-        const vurdering = JSON.parse(fs.readFileSync(mockfile, "utf8"));
-        return res.json(vurdering);
+      const behandlingID = req.params.behandlingID;
+      const mockfile = `${MOCK_DATA_DIR}/vurdering/vurdering-bid-${behandlingID}.json`;
+      if (fs.existsSync(mockfile)) {
+        const data = JSON.parse(fs.readFileSync(mockfile, "utf8"));
+        const status = happyStatus([200, 200, 404]);
+        if (status === 200) {
+          data.vurdering.feilmeldinger = [];
+        }
+        return res.json(data);
+      }
+      else {
+        return res.status(404).send(ERR.notFound404(req.url));
+      }
     }
     catch (err) {
         console.error(err);
@@ -188,35 +505,48 @@ router.get('/vurdering/:behandlingID', getVurdering);
 
 router.post('/vurdering/:behandlingID', function (req, res) {
     const behandlingID = req.params.behandlingID;
-    var body = req.body;
-    var responseBody = isJSON(body) ? JSON.parse(body) : body;
+    const body = req.body;
+    let responseBody = isJSON(body) ? JSON.parse(body) : body;
     responseBody.behandlingID = behandlingID;
-
     return res.json(responseBody);
 });
 
 router.get('/saksbehandler', function (req, res) {
   try {
-    const saksbehandlere =  JSON.parse(fs.readFileSync("./scripts/mock_data/saksbehandler.json", "utf8"));
-    // return a random sakbehandler from list of sakbehandlere
-    return res.json(_.sample(saksbehandlere));
+    const mockfile = `${MOCK_DATA_DIR}/saksbehandler.json`;
+    const saksbehandlere =  JSON.parse(fs.readFileSync(mockfile, "utf8"));
+    const status = happyStatus([200, 200, 200, 401, 500]);
+    const url = '/saksbehandler';
+    switch (status) {
+      case 200:
+        return res.json(_.sample(saksbehandlere));
+      case 401: {
+        const melding = ERR.unauthorizedRequest401(url);
+        return res.status(status).send(melding);
+      }
+      case 500: {
+        const melding = ERR.serverError500(url);
+        return res.status(status).send(melding);
+      }
+    }
   } catch (err) {
-    console.log(err)
+    console.log(err);
+    return res.status(500).send(err);
   }
 });
-
 router.get('/landkoder', function (req, res) {
   try {
-    const land =  JSON.parse(fs.readFileSync("./scripts/mock_data/landkoder/landkoder.json", "utf8"));
+    const mockfile = `${MOCK_DATA_DIR}/landkoder/landkoder.json`;
+    const land =  JSON.parse(fs.readFileSync(mockfile, "utf8"));
     return res.json(land);
   } catch (err) {
-    console.log(err)
+    console.log(err);
+    return res.status(500).send(err);
   }
 });
 app.use(allowCrossDomain);
 app.use('/melosys-api', router);
 app.use('/api', router);
-app.use('/melosys/api', router);
 
 app.listen(port);
 
