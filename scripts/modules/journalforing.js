410d5dccde84ba9b91b67667620aa3f176df611f
Terje Karlsen <terjeofnorway@gmail.com>
2018-04-20 09:15:40 +0200

    Fjernet sensitiv info. Er ikke påkrevet.

--- scripts/modules/journalforing.js
+++ scripts/modules/journalforing.js
@@ -149,7 +149,6 @@ const oppgave = {
   },
   behandlingstyper: Kodeverk.behandlingstyper,
   vedleggstittler: tittler,
-  inneholderSensitivInfo: true,
   dokumentURL: '/dokumenttest.pdf',
   saksListe: byggEksisterendeSakerMock(),
 };
