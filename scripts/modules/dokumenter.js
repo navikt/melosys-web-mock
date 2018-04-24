c971b66eb53dc7bc8deb8a2863074c76c46c4052
Bjølseth, Are <are.bjolseth@nav.no>
2018-04-24 14:57:42 +0200

    simple upload without validation or error handling.

--- scripts/modules/dokumenter.js
+++ scripts/modules/dokumenter.js
@@ -2,4 +2,7 @@ const fs = require('fs');
 const MOCK_DATA_DIR = `${process.cwd()}/scripts/mock_data`;
 
 exports.hentPdfDokument = (req, res) => {
+  const navn = req.params.navn;
+  const path = `${MOCK_DATA_DIR}/${navn}`;
+  res.download(path);
 }
