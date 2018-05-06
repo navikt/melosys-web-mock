ed348fd07b041bb498f5aa0c7a300cc19ae81f4f
abjolseth <abjolseth@gmail.com>
2018-05-06 20:29:10 +0200

    Spike for evaluering av schema validator

--- scripts/kodeverk.js
+++ scripts/kodeverk.js
@@ -1,15 +1,15 @@
+
 const fs = require('fs');
 const Ajv = require('ajv');
 const ajv = new Ajv({allErrors: true});
 
 const MOCK_DATA_DIR = `${process.cwd()}/mock_data`;
-const SOKNADER_MOCK_DIR = `${MOCK_DATA_DIR}/soknader`;
 
-const schemajson = `${SOKNADER_MOCK_DIR}/soknad-schema.json`;
+const schemajson = `${MOCK_DATA_DIR}/kodeverk-schema.json`;
 const schema = JSON.parse(fs.readFileSync(schemajson, "utf8"));
 const validate = ajv.compile(schema);
 
-const dokumentjson = `${SOKNADER_MOCK_DIR}/soknad-bid-4.json`;
+const dokumentjson = `${MOCK_DATA_DIR}/kodeverk.json`;
 const dokument = JSON.parse(fs.readFileSync(dokumentjson, "utf8"));
 
 test(dokument);
