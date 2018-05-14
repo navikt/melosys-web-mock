419c7687a15193a70dc2236557f50cfac6665d67
Terje Karlsen <terjeofnorway@gmail.com>
2018-05-14 13:56:20 +0200

    Fikset id på validerings-schema. Refactor fra download til sendFile.

--- scripts/modules/dokumenter.js
+++ scripts/modules/dokumenter.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 const MOCK_DATA_DIR = `${process.cwd()}/scripts/mock_data`;
 
 exports.hentPdf = (req, res) => {
-  //const journalpostID = req.params.journalpostID;
-  //const dokumentID = req.params.dokumentID;
   const mockfile = `${MOCK_DATA_DIR}/dokumenter/dokumenttest.pdf`;
-  res.download(mockfile);
+
+  res.type('application/pdf');
+  res.sendFile(mockfile);
 };
