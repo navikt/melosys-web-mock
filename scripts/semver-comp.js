e842536d4644b7fda4ff863bc7533352981f6481
Bjølseth, Are <are.bjolseth@nav.no>
2018-08-10 15:11:08 +0200

    Work in progress

--- scripts/semver-comp.js
+++ scripts/semver-comp.js
@@ -1,5 +1,8 @@
 const semver = require('semver');
-const argv = require('yargs').argv;
-// yarn semver-comp -a 2.0.0 -b 1.0.0
-const comp = semver.gt(argv.a, argv.b);
+const argv = process.argv;
+// argv.map((e, i) => console.log(i, e));
+const a = argv[3];
+const b = argv[5];
+// npm run-script semver-comp -- -a 2.0.0 -b 1.0.0
+const comp = semver.gt(a, b);
 console.log(comp);
