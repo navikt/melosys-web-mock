b36f52498aad68fea0c279f03232527be4ccccea
Terje Karlsen <terjeofnorway@gmail.com>
2018-08-14 16:55:26 +0200

    Lagt til schema i package. Byttet rekkefølge på demo og kodeverk.

--- scripts/schema.js
+++ scripts/schema.js
@@ -3,5 +3,5 @@ const { kodeverk } = require('./test/kodeverk');
 // const { person } = require('./test/person');
 // const { soknad } = require('./test/soknad');
 
-kodeverk.test();
 demo.test();
+kodeverk.test();
