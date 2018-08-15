d7bb31214733ed809cac4ad7a9a5f9c207db767e
abjolseth <abjolseth@gmail.com>
2018-08-16 00:15:02 +0200

    Added functionality for watching changes in data files and trigger recompile of schemas.

--- scripts/schema.js
+++ scripts/schema.js
@@ -10,16 +10,47 @@ const { organisasjon } = require('./test/organsisasjon');
 const { inngang } = require('./test/inngang');
 const { journalforing } = require('./test/journalforing');
 
-demo.test();
-kodeverk.test();
-person.test();
-soknad.test();
-vurdering.test();
-fagsak.test();
-SokFagsak.test();
-Saksbehandler.test();
-organisasjon.test();
-inngang.test();
-journalforing.test();
 
+const MOCK_DATA_DIR = `${process.cwd()}/scripts/mock_data`;
+const GLOBBER = `${MOCK_DATA_DIR}/*/*.json`;
+const watcher = require('chokidar').watch(GLOBBER, {
+  ignored: /(^|[\/\\])\../,
+  persistent: true
+});
+
+const testAll = () => {
+  demo.test();
+  kodeverk.test();
+  person.test();
+  soknad.test();
+  vurdering.test();
+  fagsak.test();
+  SokFagsak.test();
+  Saksbehandler.test();
+  organisasjon.test();
+  inngang.test();
+  journalforing.test();
+};
+
+// Something to use when events are received.
+const log = console.log.bind(console);
+// See https://www.npmjs.com/package/chokidar
+watcher
+.on('add', path => {
+  const txt = path.slice(MOCK_DATA_DIR.length);
+  log(`Watching ${txt}`);
+})
+.on('change', path => {
+  log(`File ${path} has been changed`);
+  testAll();
+})
+.on('raw', (event, path) => {
+  //log('Raw event info:', event, path, details);
+  const txt = path.slice(MOCK_DATA_DIR.length);
+  log('--------------------------------');
+  log(`Touched ${txt}`);
+  testAll();
+});
+
+testAll();
 console.log('\nSchema validation completed.');
