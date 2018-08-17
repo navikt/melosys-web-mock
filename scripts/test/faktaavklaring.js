9d1cd67a03ea930525261d545f25228035c1e964
Bjølseth, Are <are.bjolseth@nav.no>
2018-08-17 14:27:50 +0200

    Fix module.exports.faktaavklaring

--- scripts/test/faktaavklaring.js
+++ scripts/test/faktaavklaring.js
@@ -1,7 +1,6 @@
 const Ajv = require('ajv');
 const ajv = new Ajv({allErrors: true});
 const colors = require('colors/safe');
-const fs = require('fs');
 
 const Utils = require('../modules/utils');
 const { lesFaktaavklaringKatalog } = require('../modules/faktaavklaring');
@@ -9,8 +8,9 @@ const { lesFaktaavklaringKatalog } = require('../modules/faktaavklaring');
 const SCRIPTS_DIR =`${process.cwd()}/scripts`;
 const SCHEMA_DIR = `${SCRIPTS_DIR}/schema`;
 
-const schemajson = `${SCHEMA_DIR}/faktaavklaring-schema.json`;
-const schema = JSON.parse(fs.readFileSync(schemajson, "utf8"));
+const schemapath = `${SCHEMA_DIR}/faktaavklaring-schema.json`;
+const schema = Utils.lesSchema(schemapath);
+
 const catalog = lesFaktaavklaringKatalog();
 
 const validate = ajv.compile(schema);
@@ -21,8 +21,8 @@ const test = () => {
   catalog.forEach((elem) => Utils.runTest(elem, ajv, validate));
 };
 
-const fagsak = {
+const faktaavklaring = {
   test,
 };
-module.exports.fagsak = fagsak;
+module.exports.faktaavklaring = faktaavklaring;
 
