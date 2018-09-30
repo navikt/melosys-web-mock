36033acdb5460c25a40f8d4dcfce348bff28eac6
abjolseth <abjolseth@gmail.com>
2018-09-30 15:37:34 +0200

    Async hent saksbehandler

--- scripts/modules/saksbehandler.js
+++ scripts/modules/saksbehandler.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
 const log4js = require('log4js');
 const logger = log4js.getLogger('mock');
 const _ = require('underscore');
 
+const Utils = require('./utils');
 const Schema = require('../test/schema-util');
 const ERR = require('./errors');
 const happy = require('./happystatus');
@@ -12,14 +12,14 @@ const MOCK_DATA_SAKSBEHANDLER_DIR = `${MOCK_DATA_DIR}/saksbehandler`;
 module.exports.lesSaksbehandlerKatalog = () => {
   return Schema.lesKatalog(MOCK_DATA_SAKSBEHANDLER_DIR);
 };
-const lesSaksbehandlere = () => {
+const lesSaksbehandlere = async () => {
   const mockfile = `${MOCK_DATA_DIR}/saksbehandler.json`;
-  return fs.existsSync(mockfile) ? JSON.parse(fs.readFileSync(mockfile, "utf8")) : {};
+  return await Utils.existsAsync(mockfile) ? JSON.parse(await Utils.readFileAsync(mockfile)) : {};
 };
 
-module.exports.hent = (req, res) => {
+module.exports.hent = async (req, res) => {
   try {
-    const saksbehandlere = lesSaksbehandlere();
+    const saksbehandlere = await lesSaksbehandlere();
     const status = happy.happyStatus([200, 200, 200, 401, 500]);
     const url = '/saksbehandler';
     switch (status) {
