4cd89313dddcbf6d438640faf3ebfd4cd8353bc8
Terje Karlsen <terjeofnorway@gmail.com>
2018-10-25 17:20:17 +0200

    Fikset div skrivefeil og punktsetting.

--- scripts/modules/kodeverk/begrunnelser/artikkel16.1_anmodning.js
+++ scripts/modules/kodeverk/begrunnelser/artikkel16.1_anmodning.js
@@ -10,35 +10,35 @@ const artikkel16_1_anmodning = [
   },
   {
     "kode": "MOTTAR_LOENN_UTL.KONSERN",
-    "term": "Lønnes av utenlandsk foretak som er del av samme konsern"
+    "term": "Lønnes av utenlandsk foretak som er del av samme konsern."
   },
   {
     "kode": "HJEMMEKONTOR_UNDER_12_MN",
-    "term": "Arbeider på hjemmekontor under 12 mn"
+    "term": "Arbeider på hjemmekontor under 12 mnd."
   },
   {
     "kode": "HJEMMEKONTOR_MEDFOELGENDE",
-    "term": "Medfølgende som arbeider på hjemmekontor"
+    "term": "Medfølgende som arbeider på hjemmekontor."
   },
   {
     "kode": "MEDFOELGENDE_LITEN_STILLINGSPROSENT",
-    "term": "Medfølgende som arbeider lokalt med liten stillingsprosent"
+    "term": "Medfølgende som arbeider lokalt med liten stillingsprosent."
   },
   {
     "kode": "IDEELL_ORGANISASJON_ULOENNET",
-    "term": "Frivillig arbeid for ideell organisasjon"
+    "term": "Frivillig arbeid for ideell organisasjon."
   },
   {
     "kode": "IDEELL_ORGANISASJON_LOENNET_IKKE_VESENTLIG_VIRK",
-    "term": "Arbeider for ideell organisasjon som ikke har vesentlig virksomhet i Norge"
+    "term": "Arbeider for ideell organisasjon som ikke har vesentlig virksomhet i Norge."
   },
   {
     "kode": "STUDENT_MED_KORTVARIG_STUDIERELATERT_ARBEID",
-    "term": "Student med kortvarig studierelatert arbeid i vertslandet"
+    "term": "Student med kortvarig studierelatert arbeid i vertslandet."
   },
   {
     "kode": "ARBEIDSKONTRAKT_VERTSLAND_STIPEND_FRA_NORSKE_STAT",
-    "term": "Arbeidskontrakt i vertslandet og stipend fra Den norske stat"
+    "term": "Arbeidskontrakt i vertslandet og stipend fra Den norske stat."
   }
 ];
 module.exports.artikkel16_1_anmodning = artikkel16_1_anmodning;
