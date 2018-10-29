4c62ec621039a7a897e23ab3a6a47e61071597ff
Bjørnar Askevold <bjornar.askevold@nav.no>
2018-10-29 14:28:13 +0100

    Lagt til validering av post json

--- scripts/modules/oppgaver.js
+++ scripts/modules/oppgaver.js
@@ -1,8 +1,21 @@
+const Ajv = require('ajv');
+const ajv = new Ajv({allErrors: true});
 const log4js = require('log4js');
 const logger = log4js.getLogger('mock');
 const _ = require('underscore');
 
 const Utils = require('./utils');
+const Schema = require('../test/schema-util');
+const ERR = require('./errors');
+
+const SCRIPTS_DATA_DIR = `${process.cwd()}/scripts`;
+const SCHEMA_DIR = `${SCRIPTS_DATA_DIR}/schema`;
+const definitionsPath = `${SCHEMA_DIR}/definitions-schema.json`;
+const definitions = Schema.lesSchemaSync(definitionsPath);
+
+const schemajson = `${SCHEMA_DIR}/oppgaver-tilbakelegg-schema.json`;
+const schema = Schema.lesSchemaSync(schemajson);
+const validateTilbakelegg = ajv.addSchema(definitions).compile(schema);
 
 const MOCK_DATA_DIR  = `${process.cwd()}/scripts/mock_data`;
 const MOCK_DATA_OPPGAVRE_DIR = `${MOCK_DATA_DIR}/oppgaver`;
@@ -90,6 +103,38 @@ module.exports.reset = (req, res) => {
 module.exports.tilbakelegg = (req, res) => {
   const body = req.body;
   const jsBody = Utils.isJSON(body) ? JSON.parse(body) : body;
+
+  try {
+    const valid = test(validateTilbakelegg, jsBody);
+    if (!valid) {
+      valideringFeil(req, res);
+    }
+    res.json(body);
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+
   logger.debug("oppgaver:tilbakelegg", JSON.stringify(jsBody));
   res.status(204).send();
 };
+
+function valideringFeil(req, res) {
+  const status = 400;
+  const melding = ERR.errorMessage(400,'Bad Request', 'Invalid schema', req.originalUrl);
+  res.status(status).send(melding);
+}
+
+function test(validate, data) {
+  const valid = validate(data);
+  if (valid) {
+    console.log('TilbakeleggeOppgave: send Valid!');
+  }
+  else {
+    const ajvErros = ajv.errorsText(validate.errors);
+    console.error('Oppgave:send INVALID: see mock-errors.log');
+    logger.error('Oppgave:send INVALID', ajvErros)
+  }
+  return valid;
+}
