22f60b312d06bd2de7a5ef88f97da65ee7e6aa20
Eivind K. Nilsbakken <eivind.nilsbakken@nav.no>
2018-10-30 15:03:32 +0100

    Legg til fellesdefinisjon av behandlingID

--- scripts/test/vurdering.js
+++ scripts/test/vurdering.js
@@ -7,11 +7,13 @@ const Vurderinger = require('../modules/vurdering');
 const SCRIPTS_DIR =`${process.cwd()}/scripts`;
 const SCHEMA_DIR = `${SCRIPTS_DIR}/schema`;
 
+const definitionsPath = `${SCHEMA_DIR}/definitions-schema.json`;
+const definitions = Schema.lesSchemaSync(definitionsPath);
 const schemajson = `${SCHEMA_DIR}/vurdering-schema.json`;
 const schema = Schema.lesSchemaSync(schemajson);
 const katalog = Vurderinger.lesVurderingsKatalog();
 
-const validate = ajv.compile(schema);
+const validate = ajv.addSchema(definitions).compile(schema);
 
 
 const testAll = () => {
