18266af25502502a8049a8cea6282c98f9b09ac4
Terje Karlsen <terjeofnorway@gmail.com>
2018-11-16 11:33:39 +0100

    Lagt til begrunnelse i kodeverk for 12.2.

--- scripts/modules/kodeverk/begrunnelser/index.js
+++ scripts/modules/kodeverk/begrunnelser/index.js
@@ -6,6 +6,7 @@ const { opphold } = require('./opphold');
 const { vesentligVirksomhet12_1 } = require('./vesentligVirksomhet12_1');
 const { vesentligVirksomhet12_2 } = require('./vesentligVirksomhet12_2');
 const { artikkel12_1 } = require('./artikkel12.1');
+const { artikkel12_2 } = require('./artikkel12.2');
 const { artikkel16_1_anmodning } = require('./artikkel16.1_anmodning');
 
 const begrunnelser = {
@@ -16,6 +17,7 @@ const begrunnelser = {
   vesentligVirksomhet12_1,
   vesentligVirksomhet12_2,
   artikkel12_1,
+  artikkel12_2,
   artikkel16_1_anmodning,
 };
 
