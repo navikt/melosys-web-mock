0111c08ee5a4c9e60fffecb77ab93c394fc5e512
Terje Karlsen <terjeofnorway@gmail.com>
2018-11-28 12:41:18 +0100

    Diverse kode-oppdateringer.

--- scripts/modules/kodeverk/vedleggstitler.js
+++ scripts/modules/kodeverk/vedleggstitler.js
@@ -2,18 +2,40 @@
  * Kodeverk/vedleggstitler
  * @module
  */
+
+// Etter møte med fag 27. nov 18 er det avklart at titler for hoveddokument og veglegg skal være det samme.
 const vedleggstitler = [
   {
-    kode: 'TITTEL_1',
-    term: 'Vedleggstittel 1'
+    kode:'ARBF',
+    term:'Arbeidsforhold'
   },
   {
-    kode:'TITTEL_2',
-    term: 'Vedleggstittel 2'
+    kode:'BKR_MEDL',
+    term:'Bekreftelse på medlemskap i folketrygden'
   },
   {
-    kode: 'ANNET',
-    term: 'Annet...'
+    kode:'INNT_SKAT',
+    term:'Inntektsopplysninger'
+  },
+  {
+    kode:'MERK',
+    term:'Merknad til sak'
+  },
+  {
+    kode:'STUDIE_DOKUMENTASJON',
+    term:'Studiedokumentasjon'
+  },
+  {
+    kode:'SOK_MED',
+    term:'Søknad om medlemskap'
+  },
+  {
+    kode:'BEKR_UNNT_FRA_MEDL',
+    term:'Unntak'
+  },
+  {
+    kode:'ANNET',
+    term:'Annet (=fritekst)'
   }
 ];
 module.exports.vedleggstitler = vedleggstitler;
