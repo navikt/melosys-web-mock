bbcfc66cc8a45b768ef8d84e28c67198106896fa
Bjølseth, Are <are.bjolseth@nav.no>
2018-11-30 10:22:42 +0100

    La til schema validering for api/behandlinger/4/status

--- scripts/modules/behandlinger.js
+++ scripts/modules/behandlinger.js
@@ -1,9 +1,22 @@
 const log4js = require('log4js');
 const logger = log4js.getLogger('mock');
-const ERR = require('./errors');
+const Ajv = require('ajv');
+
 const Utils = require('./utils');
+const Schema = require('../test/schema-util');
+const ERR = require('./errors');
+
+const SCRIPTS_DIR = `${process.cwd()}/scripts`;
+const SCHEMA_DIR = `${SCRIPTS_DIR}/schema`;
+const definitionsPath = `${SCHEMA_DIR}/definitions-schema.json`;
+const definitions = Schema.lesSchemaSync(definitionsPath);
 
 module.exports.status = (req, res) => {
+  const schemajson = `${SCHEMA_DIR}/behandlinger-status-post-schema.json`;
+  const schema = Schema.lesSchemaSync(schemajson);
+  const ajv = new Ajv({allErrors: true});
+  const ajvValidator = ajv.addSchema(definitions).compile(schema);
+
   try {
     const { behandlingID } = req.params;
     if (!behandlingID) {
@@ -12,8 +25,14 @@ module.exports.status = (req, res) => {
     }
     const { body } = req;
     const jsBody = Utils.isJSON(body) ? JSON.parse(body) : body;
-    console.log(jsBody);
-    res.status(204).send();
+    const valid = test(ajvValidator, jsBody, ajv);
+
+    if (valid) {
+      res.status(204).send();
+    }
+    else {
+      valideringFeil(req, res);
+    }
   }
   catch (err) {
     console.log(err);
@@ -22,3 +41,17 @@ module.exports.status = (req, res) => {
     res.status(500).send(melding);
   }
 };
+
+
+function valideringFeil(req, res) {
+  const status = 400;
+  const melding = ERR.errorMessage(400,'Bad Request', 'Invalid schema', req.originalUrl);
+  res.status(status).send(melding);
+}
+
+function test(ajvValidator, data, ajv) {
+  const valid = ajvValidator(data);
+  if (valid) console.log('Behandlinger:status Valid!');
+  else console.log('Invalid: ' + ajv.errorsText(ajvValidator.errors));
+  return valid;
+}
