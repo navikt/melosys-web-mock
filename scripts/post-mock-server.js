5e79b6afec0de57839c1fd0bdcb6fab4200ab0c1
Bjølseth, Are <are.bjolseth@nav.no>
2018-12-12 16:30:19 +0100

    Oppdaterte post mock med aync/await og oppsummering.

--- scripts/post-mock-server.js
+++ scripts/post-mock-server.js
@@ -7,7 +7,12 @@ const instance = axios.create({
   timeout: 1000
 });
 
+const oppsummering = {
+  success: 0,
+  failure: 0,
+};
 const printresult = res => {
+  oppsummering.success += 1;
   const { method, url} = res.config;
   console.log(`[${method.toUpperCase()}]`, url);
   console.log(res.status, res.statusText);
@@ -16,6 +21,7 @@ const printresult = res => {
 };
 
 const printerror = res => {
+  oppsummering.failure += 1;
   const { request, response } = res;
   console.error(`[${request.method.toUpperCase()}]`, request.path);
   console.error(colors.bgRed(`${response.status} ${response.statusText}`));
@@ -23,45 +29,56 @@ const printerror = res => {
   console.error("-------------------------------------------------------\n");
 };
 
+const testAlleEndepunkter = async () => {
+  try {
 
-instance.get('/saksbehandler').then(printresult).catch(printerror);
+    await await instance.get('/saksbehandler').then(printresult).catch(printerror);
 
-const behandinger_status = require('./mock_data/behandlinger/post/behandlinger-status');
-instance.post('/behandlinger/4/status', behandinger_status).then(printresult).catch(printerror);
+    const behandinger_status = require('./mock_data/behandlinger/post/behandlinger-status');
+    await instance.post('/behandlinger/4/status', behandinger_status).then(printresult).catch(printerror);
 
-const behandlinger_perioder= require('./mock_data/behandlinger/post/behandlinger-perioder');
-instance.post('/behandlinger/4/perioder', behandlinger_perioder).then(printresult).catch(printerror);
+    const behandlinger_perioder = require('./mock_data/behandlinger/post/behandlinger-perioder');
+    await instance.post('/behandlinger/4/perioder', behandlinger_perioder).then(printresult).catch(printerror);
 
-const soknad = require('./mock_data/soknader/post/soknad-post');
-instance.post('/soknader/4', soknad).then(printresult).catch(error => printerror(error.response.data));
+    const soknad = require('./mock_data/soknader/post/soknad-post');
+    await instance.post('/soknader/4', soknad).then(printresult).catch(error => printerror(error.response.data));
 
-const avklartefakta4 = require('./mock_data/avklartefakta/avklartefakta-bid-4');
-instance.post('/avklartefakta/4', avklartefakta4).then(printresult).catch(printerror);
+    const avklartefakta4 = require('./mock_data/avklartefakta/avklartefakta-bid-4');
+    await instance.post('/avklartefakta/4', avklartefakta4).then(printresult).catch(printerror);
 
-const lovvalgsperioder = require('./mock_data/lovvalgsperioder/lovvalgsperiode-bid-4');
-instance.post('/lovvalgsperioder/4', lovvalgsperioder).then(printresult).catch(printerror);
+    const lovvalgsperioder = require('./mock_data/lovvalgsperioder/lovvalgsperiode-bid-4');
+    await instance.post('/lovvalgsperioder/4', lovvalgsperioder).then(printresult).catch(printerror);
 
-const oversikt = require('./mock_data/oppgaver/oversikt');
-instance.post('/oppgaver/opprett', oversikt).then(printresult).catch(printerror);
+    const oversikt = require('./mock_data/oppgaver/oversikt');
+    await instance.post('/oppgaver/opprett', oversikt).then(printresult).catch(printerror);
 
-const tilbakelegg = require('./mock_data/oppgaver/post/tilbakelegge');
-instance.post('/oppgaver/tilbakelegge', tilbakelegg).then(printresult).catch(printerror);
+    const tilbakelegg = require('./mock_data/oppgaver/post/tilbakelegge');
+    await instance.post('/oppgaver/tilbakelegge', tilbakelegg).then(printresult).catch(printerror);
 
-const journal_post_opprett = require('./mock_data/journalforing/post/opprett');
-instance.post('/journalforing/opprett', journal_post_opprett).then(printresult).catch(printerror);
+    const journal_post_opprett = require('./mock_data/journalforing/post/opprett');
+    await instance.post('/journalforing/opprett', journal_post_opprett).then(printresult).catch(printerror);
 
-const journal_post_tilordne = require('./mock_data/journalforing/post/tilordne');
-instance.post('/journalforing/tilordne', journal_post_tilordne).then(printresult).catch(printerror);
+    const journal_post_tilordne = require('./mock_data/journalforing/post/tilordne');
+    await instance.post('/journalforing/tilordne', journal_post_tilordne).then(printresult).catch(printerror);
 
-const vedtak_post = require('./mock_data/vedtak/post/vedtak-post');
-instance.post('/vedtak/4', vedtak_post).then(printresult).catch(printerror);
+    const vedtak_post = require('./mock_data/vedtak/post/vedtak-post');
+    await instance.post('/vedtak/4', vedtak_post).then(printresult).catch(printerror);
 
-const dokument_post_utkast = require('./mock_data/dokumenter/post/post_utkast_og_opprett');
-const behandlingID = 3;
-const dokumentTypeID = 'MELDING_MANGLENDE_OPPLYSNINGER';
-instance.post(`/dokumenter/utkast/pdf/${behandlingID}/${dokumentTypeID}`, dokument_post_utkast).then(printresult).catch(printerror);
-instance.post(`/dokumenter/opprett/${behandlingID}/${dokumentTypeID}`, dokument_post_utkast).then(printresult).catch(printerror);
+    const dokument_post_utkast = require('./mock_data/dokumenter/post/post_utkast_og_opprett');
+    const behandlingID = 3;
+    const dokumentTypeID = 'MELDING_MANGLENDE_OPPLYSNINGER2';
+    await instance.post(`/dokumenter/utkast/pdf/${behandlingID}/${dokumentTypeID}`, dokument_post_utkast).then(printresult).catch(printerror);
+    await instance.post(`/dokumenter/opprett/${behandlingID}/${dokumentTypeID}`, dokument_post_utkast).then(printresult).catch(printerror);
 
+
+    console.dir(oppsummering);
+  }
+  catch (e) {
+    console.error(e);
+  }
+};
+
+testAlleEndepunkter();
 /*
 PUT vs POST for Creation
 In short, favor using POST for resource creation.
