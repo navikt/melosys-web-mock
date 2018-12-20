72aa67892e8658d62972510d2f1abb7a63743a26
Terje Karlsen <terjeofnorway@gmail.com>
2018-12-20 07:22:33 +0100

    Fikset feil koder i begrunnelse for sokkel / skip.

--- scripts/modules/kodeverk/begrunnelser/sokkelEllerSkip.js
+++ scripts/modules/kodeverk/begrunnelser/sokkelEllerSkip.js
@@ -5,11 +5,11 @@
  */
 const sokkelEllerSkip = [
   {
-    'kode': 'IKKE_EGET_FREMDRIFT',
+    'kode': 'IKKE_EGEN_FREMDRIFT',
     'term': 'Ikke framdrift for egen maskin'
   },
   {
-    'kode': 'IKKE_ORDINAERT_SKIPSFART',
+    'kode': 'IKKE_ORDINAER_SKIPSFART',
     'term': 'Skrogtypen kan ikke brukes i ordinær skipsfart'
   },
   {
