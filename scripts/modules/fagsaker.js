40ff749d9491540a40597f1f42dacf9d8780a6fa
JohanSakshaug <46343153+JohanSakshaug@users.noreply.github.com>
2019-01-23 12:11:47 +0100

    Melosys 2048 dialog for aa henlegge behandling (#170)
    * Legg til endpoint for henlegging av fagsak
    
    * Legg til skjema for henlegg fagsak
    
    * Bruk skjema til validering

--- scripts/modules/fagsaker.js
+++ scripts/modules/fagsaker.js
@@ -1,9 +1,18 @@
+const URL = require('url');
 const log4js = require('log4js');
 const logger = log4js.getLogger('mock');
+const Ajv = require('ajv');
+
+const Utils = require('./utils');
+const Schema = require('../test/schema-util');
+const ERR = require('./errors');
+
+const SCRIPTS_DIR = `${process.cwd()}/scripts`;
+const SCHEMA_DIR = `${SCRIPTS_DIR}/schema`;
+const definitionsPath = `${SCHEMA_DIR}/definitions-schema.json`;
+const definitions = Schema.lesSchemaSync(definitionsPath);
 
 const { existsAsync, readFileAsync } = require('./utils');
-const ERR = require('./errors');
-const Schema = require('../test/schema-util');
 
 const MOCK_DATA_DIR = `${process.cwd()}/scripts/mock_data`;
 const MOCK_DATA_FAGSAK_DIR = `${MOCK_DATA_DIR}/fagsaker`;
@@ -63,3 +72,45 @@ module.exports.opprett = (req, res) => {
   res.status(410).send(melding);
 };
 
+module.exports.henlegg = async (req, res) => {
+  const schemajson = `${SCHEMA_DIR}/henlegg-fagsak-schema.json`;
+  const schema = Schema.lesSchemaSync(schemajson);
+  const ajv = new Ajv({allErrors: true});
+
+  const ajvValidator = ajv.addSchema(definitions).compile(schema);
+  const url = URL.parse(req.url);
+  const body = req.body;
+
+  try {
+    const jsBody = Utils.isJSON(body) ? JSON.parse(body) : body;
+    const valid = test(ajvValidator, jsBody, ajv);
+
+    if (valid) {
+      res.status(204).send();
+    }
+    else {
+      valideringFeil(req, res);
+    }
+  }
+  catch(err) {
+    console.log(err);
+    logger.error(err);
+    const melding = ERR.serverError500(url, err);
+    res.status(500).send(melding);
+  }
+};
+
+function valideringFeil(req, res) {
+  const status = 400;
+  const melding = ERR.errorMessage(400,'Bad Request', 'Invalid schema', req.originalUrl);
+  res.status(status).send(melding);
+}
+
+function test(ajvValidator, data, ajv) {
+  const valid = ajvValidator(data);
+  if (valid) console.log('Vedtak:fattet Valid!');
+  else {
+    console.log('Invalid: ' + ajv.errorsText(ajvValidator.errors));
+  }
+  return valid;
+}
