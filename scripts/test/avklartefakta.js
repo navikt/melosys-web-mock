eb32bc08a5ab9d1c89b0089189e8600bacec8fc5
Are Bjolseth <abjolseth@gmail.com>
2019-03-27 17:22:39 +0100

    Commented out customSchemaValidator() with support for definitions-avklarta-schema.json

--- scripts/test/avklartefakta.js
+++ scripts/test/avklartefakta.js
@@ -2,25 +2,26 @@ const Ajv = require('ajv');
 
 const Schema = require('../utils/schema-util');
 const { lesAvklartefaktaKatalog, lesAvklartefaktaPostMock } = require('../modules/avklartefakta');
-
-const schemaValidator = () => {
+/*
+const customSchemaValidator = () => {
   const schema = Schema.lesSchemaFileSync('avklartefakta-schema.json');
   const definitions_avklartefakta_schema = Schema.lesSchemaFileSync('definitions-avklartefakta-schema.json');
   const ajv = new Ajv({allErrors: true});
   return ajv.addSchema(definitions_avklartefakta_schema).compile(schema);
 };
-
+*/
+const validate = Schema.schemaValidator('avklartefakta-schema.json');
 const testAvklaringPost = () => {
   Schema.prettyTittel('Avklartefakta/post');
   const elem = lesAvklartefaktaPostMock();
-  const validate = schemaValidator();
+  // const validate = customSchemaValidator();
   return Schema.runTest(elem, validate);
 };
 
 
 const testAll = () => {
   const catalog = lesAvklartefaktaKatalog();
-  const validate = schemaValidator();
+  // const validate = customSchemaValidator();
   Schema.prettyTittel('Avklartefakta');
   catalog.forEach(elem => Schema.runTest(elem, validate));
 
@@ -31,7 +32,7 @@ const testOne = (path) => {
   const tittel = Schema.katalogTittel(path);
   Schema.prettyTittel(tittel);
   const elem = Schema.lesKatalogElement(path);
-  const validate = schemaValidator();
+  // const validate = customSchemaValidator();
   return Schema.runTest(elem, validate);
 };
 
