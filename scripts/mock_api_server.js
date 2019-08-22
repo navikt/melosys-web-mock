770cb9fd249648cfbca297e5292568771985486d
Bjølseth, Are <are.bjolseth@nav.no>
2019-08-22 13:35:59 +0200

    cosmetic change

--- scripts/mock_api_server.js
+++ scripts/mock_api_server.js
@@ -197,9 +197,7 @@ router.get('/saksbehandler', Saksbehandler.hent);
  * ---------------------------------------------------------------
  */
 router.put('/saksflyt/anmodningsperioder/:behandlingID/bestill', Saksflyt.anmodningsperioder.bestill.put);
-
 router.put('/saksflyt/soknader/:behandlingID/videresend', Saksflyt.soknader.videresend.put);
-
 router.put('/saksflyt/unntaksperioder/:behandlingID/godkjenn', Saksflyt.unntaksperioder.godkjenn.put);
 router.post('/saksflyt/unntaksperioder/:behandlingID/ikkegodkjenn', Saksflyt.unntaksperioder.ikkegodkjenn.send);
 router.put('/saksflyt/unntaksperioder/:behandlingID/innhentinfo', Saksflyt.unntaksperioder.innhentinfo.put);
