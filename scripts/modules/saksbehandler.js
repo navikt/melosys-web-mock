3e29dfce06da7b012baf64e576aff44040bc4896
Are Bjolseth <abjolseth@gmail.com>
2019-09-12 13:59:06 +0200

    Updated old style explicit code with new katalog declaration based kode. NB! melosys-schema is also updated!!

--- scripts/modules/saksbehandler.js
+++ scripts/modules/saksbehandler.js
@@ -1,31 +1,14 @@
 const _ = require('lodash');
+const SchemaValidator  = require('../utils/schemavalidator');
+const Katalog = require('../katalog');
+const { moduleName } = Katalog.pathnameMap.saksbehandler;
 
-const { MOCK_DATA_DIR } = require('../../mock.config');
-const Utils = require('../utils/utils');
-const Mock = require('../utils/mock-util');
-
-/**
- *lesSaksbehandlere
- * @returns {Promise<{}>}
- */
-const lesSaksbehandlere = () => {
-  const mockfile = `${MOCK_DATA_DIR}/saksbehandler.json`;
-  return Utils.readJsonAndParseAsync(mockfile);
-};
-
-/**
- * hent
- * @param req
- * @param res
- * @returns {Promise<void>}
- */
 module.exports.hent = async (req, res) => {
-  try {
-    const saksbehandlere = await lesSaksbehandlere();
-    res.json(_.sample(saksbehandlere));
-  }
-  catch (err) {
-   Mock.serverError(req, res, err);
-  }
+  const mockSaksbehandlere = ['Z990405','Z991001'];
+  const ident = _.sample(mockSaksbehandlere);
+  const pathObject = {
+    pathname: 'saksbehandler-:ident',
+    params: { ident },
+  };
+  return SchemaValidator.get(moduleName, req, res, pathObject);
 };
-
