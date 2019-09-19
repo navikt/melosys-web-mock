43cd569a7d8a98b284cb84723859e2720838ba87
JohanSakshaug <Johan.Sakshaug@nav.no>
2019-09-19 10:25:54 +0200

    Nytt endepunkt for videresending: "api/fagsaker/{saksnummer}/henlegg-videresend"

--- scripts/modules/saksflyt/index.js
+++ scripts/modules/saksflyt/index.js
@@ -1,14 +1,10 @@
 const { bestill } = require('./anmodningsperioder');
-const { videresend } = require('./soknader');
 const { anmodning, godkjenn, innhentinfo, ikkegodkjenn } = require('./unntaksperioder');
 const { endreperiode, fatt } = require('./vedtak');
 
 module.exports = {
   anmodningsperioder: {
     bestill: { put: bestill },
-  },
-  soknader: {
-    videresend: { put: videresend },
   },
   vedtak: {
     endreperiode: {send: endreperiode},
