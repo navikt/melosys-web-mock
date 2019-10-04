23a7f0b22f6f2d08f18230c7e3ac02eadb800fb0
JohanSakshaug <Johan.Sakshaug@nav.no>
2019-10-04 14:45:20 +0200

    Returner ny payload fra plukk

--- scripts/modules/oppgaver/plukk.js
+++ scripts/modules/oppgaver/plukk.js
@@ -3,5 +3,14 @@ const Katalog = require('../../katalog');
 
 module.exports.sendPlukk = async (req, res) => {
   const { moduleName } = Katalog.pathnameMap['oppgaver-plukk'];
-  SchemaValidator.post(moduleName, req, res);
+
+  const customResponse = {
+    oppgaveID: "123456789",
+    behandlingstype: "SOEKNAD",
+    saksnummer: "4",
+    journalpostID: "DOK_123",
+    behandlingID: 4,
+  };
+
+  SchemaValidator.post(moduleName, req, res, customResponse);
 };
