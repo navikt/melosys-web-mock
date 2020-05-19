e046c976a09b461a0871eb5bd31b716320a14b07
JohanSakshaug <johan.sakshauger@nav.no>
2020-05-19 17:28:21 +0200

    Gi 400 ved POST av inngangsvilkaar, og server inngangsvilkaar i POST respons.

--- scripts/modules/vilkaar.js
+++ scripts/modules/vilkaar.js
@@ -1,7 +1,15 @@
+const MKV = require('melosys-kodeverk');
+
 const Mock = require('../utils/mock-util');
+const { MOCK_DATA_DIR } = require('../../mock.config');
 const SchemaValidator  = require('../utils/schemavalidator');
 const Katalog = require('../katalog');
+const Utils = require('../utils/utils');
+
+
 const { moduleName } = Katalog.pathnameMap.vilkaar;
+const VILKAAR_DATA_DIR = `${MOCK_DATA_DIR}/${moduleName}`;
+
 /**
  * Hent vilkar
  * @param req
@@ -21,16 +29,35 @@ module.exports.hent = async (req, res) => {
   SchemaValidator.get(moduleName, req, res, mockpathObj);
 };
 
+const lesVilkaar = async (behandlingID, vilkaarKode) => {
+  const mockfile = `${VILKAAR_DATA_DIR}/vilkaar-bid-${behandlingID}.json5`;
+  const vilkaarListe = await Utils.readJsonAndParseAsync(mockfile);
+  const vilkaar = vilkaarListe.find(enkeltVilkaar => enkeltVilkaar.vilkaar === vilkaarKode);
+
+  return vilkaar;
+};
+
 /**
  * Send vilkar
  * @param req
  * @param res
  * @returns {*}
  */
-module.exports.send = (req, res) => {
+module.exports.send = async (req, res) => {
   const { behandlingID } = req.params;
   if (!behandlingID) {
     return Mock.manglerParamBehandlingsID(req, res);
   }
-  SchemaValidator.post(moduleName, req, res);
+  if (req.body.includes(MKV.Koder.vilkaar.FO_883_2004_INNGANGSVILKAAR)) {
+    return Mock.forsokerAaSkriveTilInngangsvilkaar(req, res);
+  }
+
+  try {
+    const inngangsvilkaar = await lesVilkaar(behandlingID, MKV.Koder.vilkaar.FO_883_2004_INNGANGSVILKAAR);
+    if (!inngangsvilkaar) return Mock.serverError(req, res, new Error('Finner ikke inngangsvilkaar i lagrede vilkaar!'));
+    const customResponse = [...req.body, inngangsvilkaar];
+    SchemaValidator.post(moduleName, req, res, customResponse);
+  } catch (e) {
+    Mock.serverError(e);
+  }
 };
