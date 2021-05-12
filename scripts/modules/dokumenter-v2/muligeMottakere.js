5fdd625e2ddef3389d5282115b4b4c0a378a47ec
janitamarielle <janita.marielle.johansen@nav.no>
2021-05-12 10:08:13 +0200

    muligeMottakere-response er basert på req.body.produserbartdokument

--- scripts/modules/dokumenter-v2/muligeMottakere.js
+++ scripts/modules/dokumenter-v2/muligeMottakere.js
@@ -1,9 +1,34 @@
+const MKV = require('@navikt/melosys-kodeverk');
 const Katalog = require('../../katalog');
 const Mock = require('../../utils/mock-util');
 const { moduleName } = Katalog.pathnameMap["dokumenter-v2-mulige-mottakere"];
 
 const SchemaValidator = require('../../utils/schemavalidator');
 
+const {
+  MELDING_FORVENTET_SAKSBEHANDLINGSTID_SOKNAD,
+  MELDING_FORVENTET_SAKSBEHANDLINGSTID_KLAGE,
+  MANGELBREV_BRUKER,
+  MANGELBREV_ARBEIDSGIVER,
+  INNVILGELSE_FOLKETRYGDLOVEN_2_8
+} = MKV.Koder.brev.produserbaredokumenter;
+
+const getPathname = produserbartDokument => {
+  switch (produserbartDokument) {
+    case MELDING_FORVENTET_SAKSBEHANDLINGSTID_SOKNAD:
+      return 'mulige-mottakere-saksbehandlingstid_soknad';
+    case MELDING_FORVENTET_SAKSBEHANDLINGSTID_KLAGE:
+      return 'mulige-mottakere-saksbehandlingstid_klage';
+    case MANGELBREV_BRUKER:
+      return 'mulige-mottakere-mangelbrev_bruker';
+    case MANGELBREV_ARBEIDSGIVER:
+      return 'mulige-mottakere-mangelbrev_arbeidsgiver';
+    case INNVILGELSE_FOLKETRYGDLOVEN_2_8:
+      return 'mulige-mottakere-ftrl-innvilgelse';
+    default:
+      return 'attest_a1';
+  }
+};
 
 module.exports.hentMuligeMottakere = (req, res) => {
   const { behandlingID } = req.params;
@@ -12,7 +37,7 @@ module.exports.hentMuligeMottakere = (req, res) => {
   }
 
   const mockpathObject = {
-    pathname: 'mulige-mottakere-mangelbrev_arbeidsgiver'
+    pathname: getPathname(req.body.produserbartdokument)
   };
 
   return SchemaValidator.postFromFile(moduleName, req, res, mockpathObject, moduleName);
